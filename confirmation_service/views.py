f71037df9a0207c7281e12527b4ce92a7cfb4824
Dilbarov Uktamjon <dilbarov03@gmail.com>
2023-12-16 21:23:16 +0500

    recover password

--- confirmation_service/views.py
+++ confirmation_service/views.py
@@ -12,24 +12,17 @@ User = get_user_model()
 
 
 class CodeSendView(APIView):
-    action_serializer = {
-        'phone': SMSConfirmationSerializer,
-        'email': EmailConfirmationSerializer
-    }
     state_param = openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            'type': openapi.Schema(type=openapi.TYPE_STRING, description='Type'),
-            'phone': openapi.Schema(type=openapi.TYPE_STRING, description='Phone'),
             'email': openapi.Schema(type=openapi.TYPE_STRING, description='Email'),
         }
     )
 
     @swagger_auto_schema(request_body=state_param)
     def post(self, request):
-        _type = request.data.get('type', None)
         try:
-            serializer = self.action_serializer[_type](data=request.data)
+            serializer = EmailConfirmationSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -39,29 +32,22 @@ class CodeSendView(APIView):
 
 
 class CodeVerifyView(APIView):
-    action_serializer = {
-        'phone': SMSConfirmationSerializer,
-        'email': EmailConfirmationSerializer
-    }
     state_param = openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            'type': openapi.Schema(type=openapi.TYPE_STRING, description='Type'),
             'signature': openapi.Schema(type=openapi.TYPE_STRING, description='Signature'),
             'code': openapi.Schema(type=openapi.TYPE_STRING, description='Code'),
-            'phone': openapi.Schema(type=openapi.TYPE_STRING, description='Phone'),
             'email': openapi.Schema(type=openapi.TYPE_STRING, description='Email'),
         }
     )
 
     @swagger_auto_schema(request_body=state_param)
     def post(self, request):
-        _type = request.data.get('type', None)
         signature = request.data.get('signature', None)
         code = request.data.get('code', None)
         try:
-            instance = self.action_serializer[_type].Meta.model.objects.get(signature=signature)
-            serializer = self.action_serializer[_type](instance=instance, data=request.data)
+            instance = EmailConfirmationSerializer.Meta.model.objects.get(signature=signature)
+            serializer = EmailConfirmationSerializer(instance=instance, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.check_code(code)
             serializer.verify()
@@ -69,5 +55,5 @@ class CodeVerifyView(APIView):
         except KeyError:
             return Response(
                 {'error': "Invalid type", "types": self.action_serializer.keys()}, status=status.HTTP_404_NOT_FOUND)
-        except self.action_serializer[_type].Meta.model.DoesNotExist:
+        except EmailConfirmationSerializer.Meta.model.DoesNotExist:
             return Response({"error": "Signature not found"}, status=status.HTTP_404_NOT_FOUND)
